9ba07110ad762a3d00e98a13a26850c6bcef02ca
Kiyle Edwards <herculesOTPC@outlook.com>
2025-11-24 09:58:16 +0000

    finally fixed the dang master sheet.. do not mess with TILED!

---
+++ assets/images/tilesets/master.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="master" tilewidth="32" tileheight="32" tilecount="24336" columns="156">
+ <image source="master.png" width="4992" height="4992"/>
+</tileset>
--- assets/images/tilesets/master_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="master_tileset" tilewidth="32" tileheight="32" tilecount="24336" columns="156">
- <image source="master_tileset.png" width="4992" height="4992"/>
-</tileset>
